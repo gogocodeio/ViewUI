2918b69f38c86142be859ff0b1b01cd39cebfefc
chibing.fy <chibing.fy@alibaba-inc.com>
2021-07-06 15:53:29 +0800

    pretty src

--- src/components/slider/marker.js
+++ src/components/slider/marker.js
@@ -1,16 +1,20 @@
 export default {
-    name: 'SliderMarker',
-    props: {
-        mark: {
-            type: [String, Object]
-        }
+  name: 'SliderMarker',
+  props: {
+    mark: {
+      type: [String, Object],
     },
-    render (h) {
-        let label = typeof this.mark === 'string' ? this.mark : [this.mark.label];
+  },
+  render(h) {
+    let label = typeof this.mark === 'string' ? this.mark : [this.mark.label]
 
-        return h('div', {
-            class: 'ivu-slider-marks-item',
-            style: this.mark.style || {}
-        }, label);
-    }
-};
+    return h(
+      'div',
+      {
+        class: 'ivu-slider-marks-item',
+        style: this.mark.style || {},
+      },
+      label
+    )
+  },
+}
--- src/directives/v-click-outside-x.js
+++ src/directives/v-click-outside-x.js
@@ -1,7 +1,7 @@
-const CLICK = 'click';
-const captureInstances = Object.create(null);
-const nonCaptureInstances = Object.create(null);
-const instancesList = [captureInstances, nonCaptureInstances];
+const CLICK = 'click'
+const captureInstances = Object.create(null)
+const nonCaptureInstances = Object.create(null)
+const instancesList = [captureInstances, nonCaptureInstances]
 
 /**
  * The common event handler for bot capture and non-capture events.
@@ -12,32 +12,32 @@ const instancesList = [captureInstances, nonCaptureInstances];
  * @returns {undefined} Default.
  */
 const commonHandler = function _onCommonEvent(context, instances, event) {
-    const {target} = event;
+  const { target } = event
 
-    const itemIteratee = function _itemIteratee(item) {
-        const {el} = item;
+  const itemIteratee = function _itemIteratee(item) {
+    const { el } = item
 
-        if (el !== target && !el.contains(target)) {
-            const {binding} = item;
+    if (el !== target && !el.contains(target)) {
+      const { binding } = item
 
-            if (binding.modifiers.stop) {
-                event.stopPropagation();
-            }
+      if (binding.modifiers.stop) {
+        event.stopPropagation()
+      }
 
-            if (binding.modifiers.prevent) {
-                event.preventDefault();
-            }
+      if (binding.modifiers.prevent) {
+        event.preventDefault()
+      }
 
-            binding.value.call(context, event);
-        }
-    };
+      binding.value.call(context, event)
+    }
+  }
 
-    const keysIteratee = function _keysIteratee(eventName) {
-        return instances[eventName].forEach(itemIteratee);
-    };
+  const keysIteratee = function _keysIteratee(eventName) {
+    return instances[eventName].forEach(itemIteratee)
+  }
 
-    Object.keys(instances).forEach(keysIteratee);
-};
+  Object.keys(instances).forEach(keysIteratee)
+}
 
 /**
  * Event handler for capture events.
@@ -45,9 +45,9 @@ const commonHandler = function _onCommonEvent(context, instances, event) {
  * @param {Event} event - The event object.
  */
 const captureEventHandler = function onCaptureEvent(event) {
-    /* eslint-disable-next-line babel/no-invalid-this */
-    commonHandler(this, captureInstances, event);
-};
+  /* eslint-disable-next-line babel/no-invalid-this */
+  commonHandler(this, captureInstances, event)
+}
 
 /**
  * Event handler for non-capture events.
@@ -55,9 +55,9 @@ const captureEventHandler = function onCaptureEvent(event) {
  * @param {Event} event - The event object.
  */
 const nonCaptureEventHandler = function onNonCaptureEvent(event) {
-    /* eslint-disable-next-line babel/no-invalid-this */
-    commonHandler(this, nonCaptureInstances, event);
-};
+  /* eslint-disable-next-line babel/no-invalid-this */
+  commonHandler(this, nonCaptureInstances, event)
+}
 
 /**
  * Get the correct event handler: Capture or non-capture.
@@ -67,8 +67,8 @@ const nonCaptureEventHandler = function onNonCaptureEvent(event) {
  * @returns {Function} - The event handler.
  */
 const getEventHandler = function _getEventHandler(useCapture) {
-    return useCapture ? captureEventHandler : nonCaptureEventHandler;
-};
+  return useCapture ? captureEventHandler : nonCaptureEventHandler
+}
 
 /**
  * The directive definition.
@@ -86,119 +86,121 @@ const getEventHandler = function _getEventHandler(useCapture) {
  * @property {string} version - The version number of this release.
  */
 export const directive = Object.defineProperties(
-    {},
-    {
-        $_captureInstances: {
-            value: captureInstances,
-        },
-
-        $_nonCaptureInstances: {
-            value: nonCaptureInstances,
-        },
-
-        $_onCaptureEvent: {
-            value: captureEventHandler,
-        },
-
-        $_onNonCaptureEvent: {
-            value: nonCaptureEventHandler,
-        },
-
-        /**
-         * 注意，这里的 arg 修改为 capture，这样可以动态设置，原先的事件作为 modifiers
-         * */
-        bind: {
-            value: function bind(el, binding) {
-                if (typeof binding.value !== 'function') {
-                    throw new TypeError('Binding value must be a function.');
-                }
-
-                let eventType;
-                const modifiers = binding.modifiers;
-                if (modifiers.click) eventType = 'click';
-                else if (modifiers.mousedown) eventType = 'mousedown';
-                else if (modifiers.touchstart) eventType = 'touchstart';
-                else eventType = CLICK;
-
-                const useCapture = binding.arg;
-
-                const normalisedBinding = {
-                    ...binding,
-                    ...{
-                        modifiers: {
-                            ...{
-                                capture: false,
-                                prevent: false,
-                                stop: false,
-                            },
-                            ...binding.modifiers,
-                        },
-                    },
-                };
-
-                const instances = useCapture ? captureInstances : nonCaptureInstances;
-
-                if (!Array.isArray(instances[eventType])) {
-                    instances[eventType] = [];
-                }
-
-                if (instances[eventType].push({el, binding: normalisedBinding}) === 1) {
-                    if (typeof document === 'object' && document) {
-                        document.addEventListener(
-                            eventType,
-                            getEventHandler(useCapture),
-                            useCapture,
-                        );
-                    }
-                }
-            },
-        },
-
-        unbind: {
-            value: function unbind(el) {
-                const compareElements = function _compareElements(item) {
-                    return item.el !== el;
-                };
-
-                const instancesIteratee = function _instancesIteratee(instances) {
-                    const instanceKeys = Object.keys(instances);
-
-                    if (instanceKeys.length) {
-                        const useCapture = instances === captureInstances;
-
-                        const keysIteratee = function _keysIteratee(eventName) {
-                            const newInstance = instances[eventName].filter(compareElements);
-
-                            if (newInstance.length) {
-                                instances[eventName] = newInstance;
-                            } else {
-                                if (typeof document === 'object' && document) {
-                                    document.removeEventListener(
-                                        eventName,
-                                        getEventHandler(useCapture),
-                                        useCapture,
-                                    );
-                                }
-
-                                delete instances[eventName];
-                            }
-                        };
-
-                        instanceKeys.forEach(keysIteratee);
-                    }
-                };
-
-                instancesList.forEach(instancesIteratee);
-            },
-        },
-
-        /* Note: This needs to be manually updated to match package.json. */
-        version: {
-            enumerable: true,
-            value: '3.7.1',
-        },
+  {},
+  {
+    $_captureInstances: {
+      value: captureInstances,
     },
-);
+
+    $_nonCaptureInstances: {
+      value: nonCaptureInstances,
+    },
+
+    $_onCaptureEvent: {
+      value: captureEventHandler,
+    },
+
+    $_onNonCaptureEvent: {
+      value: nonCaptureEventHandler,
+    },
+
+    /**
+     * 注意，这里的 arg 修改为 capture，这样可以动态设置，原先的事件作为 modifiers
+     * */
+    bind: {
+      value: function bind(el, binding) {
+        if (typeof binding.value !== 'function') {
+          throw new TypeError('Binding value must be a function.')
+        }
+
+        let eventType
+        const modifiers = binding.modifiers
+        if (modifiers.click) eventType = 'click'
+        else if (modifiers.mousedown) eventType = 'mousedown'
+        else if (modifiers.touchstart) eventType = 'touchstart'
+        else eventType = CLICK
+
+        const useCapture = binding.arg
+
+        const normalisedBinding = {
+          ...binding,
+          ...{
+            modifiers: {
+              ...{
+                capture: false,
+                prevent: false,
+                stop: false,
+              },
+              ...binding.modifiers,
+            },
+          },
+        }
+
+        const instances = useCapture ? captureInstances : nonCaptureInstances
+
+        if (!Array.isArray(instances[eventType])) {
+          instances[eventType] = []
+        }
+
+        if (
+          instances[eventType].push({ el, binding: normalisedBinding }) === 1
+        ) {
+          if (typeof document === 'object' && document) {
+            document.addEventListener(
+              eventType,
+              getEventHandler(useCapture),
+              useCapture
+            )
+          }
+        }
+      },
+    },
+
+    unbind: {
+      value: function unbind(el) {
+        const compareElements = function _compareElements(item) {
+          return item.el !== el
+        }
+
+        const instancesIteratee = function _instancesIteratee(instances) {
+          const instanceKeys = Object.keys(instances)
+
+          if (instanceKeys.length) {
+            const useCapture = instances === captureInstances
+
+            const keysIteratee = function _keysIteratee(eventName) {
+              const newInstance = instances[eventName].filter(compareElements)
+
+              if (newInstance.length) {
+                instances[eventName] = newInstance
+              } else {
+                if (typeof document === 'object' && document) {
+                  document.removeEventListener(
+                    eventName,
+                    getEventHandler(useCapture),
+                    useCapture
+                  )
+                }
+
+                delete instances[eventName]
+              }
+            }
+
+            instanceKeys.forEach(keysIteratee)
+          }
+        }
+
+        instancesList.forEach(instancesIteratee)
+      },
+    },
+
+    /* Note: This needs to be manually updated to match package.json. */
+    version: {
+      enumerable: true,
+      value: '3.7.1',
+    },
+  }
+)
 
 /**
  * @typedef {Function} Vue - The constructor.
@@ -215,5 +217,5 @@ export const directive = Object.defineProperties(
  * @param {Vue} Vue - The Vue function.
  */
 export function install(Vue) {
-    Vue.directive('click-outside', directive);
+  Vue.directive('click-outside', directive)
 }
