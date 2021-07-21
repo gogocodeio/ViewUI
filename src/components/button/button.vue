c9674dc12c550273f22252867cdf4038f4fb7e53
wudi3 <757332281@qq.com>
2021-07-21 10:18:39 +0800

    feat: 处理vue3中<componet>  标签死循环的问题。

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,7 +1,7 @@
 <template>
-  <component
+  <a
+    v-if="tagName === 'a'"
     v-bind="tagProps"
-    :is="tagName"
     :class="classes"
     :disabled="itemDisabled"
     @click="handleClickLink"
@@ -13,67 +13,82 @@
       v-if="(icon || customIcon) && !loading"
     ></Icon>
     <span v-if="showSlot" ref="slot"><slot></slot></span>
-  </component>
+  </a>
+  <button
+    v-else
+    v-bind="tagProps"
+    :class="classes"
+    :disabled="itemDisabled"
+    @click="handleClickLink"
+  >
+    <Icon class="ivu-load-loop" type="ios-loading" v-if="loading"></Icon>
+    <Icon
+      :type="icon"
+      :custom="customIcon"
+      v-if="(icon || customIcon) && !loading"
+    ></Icon>
+    <span v-if="showSlot" ref="slot"><slot></slot></span>
+  </button>
 </template>
 
 <script>
-import Icon from '../icon'
-import { oneOf } from '../../utils/assist'
-import mixinsLink from '../../mixins/link'
-import mixinsForm from '../../mixins/form'
+import Icon from "../icon";
+import { oneOf } from "../../utils/assist";
+import mixinsLink from "../../mixins/link";
+import mixinsForm from "../../mixins/form";
 
-const prefixCls = 'ivu-btn'
+const prefixCls = "ivu-btn";
 
 export default {
-  name: 'Button',
+  name: "Button",
   mixins: [mixinsLink, mixinsForm],
   components: { Icon },
   props: {
     type: {
       validator(value) {
         return oneOf(value, [
-          'default',
-          'primary',
-          'dashed',
-          'text',
-          'info',
-          'success',
-          'warning',
-          'error',
-        ])
+          "default",
+          "primary",
+          "dashed",
+          "text",
+          "info",
+          "success",
+          "warning",
+          "error",
+        ]);
       },
-      default: 'default',
+      default: "default",
     },
     shape: {
       validator(value) {
-        return oneOf(value, ['circle', 'circle-outline'])
+        return oneOf(value, ["circle", "circle-outline"]);
       },
     },
     size: {
       validator(value) {
-        return oneOf(value, ['small', 'large', 'default'])
+        return oneOf(value, ["small", "large", "default"]);
       },
       default() {
-        return !window.$IVIEW || window.$IVIEW.size === ''
-          ? 'default'
-          : window.$IVIEW.size
+        return !window.$IVIEW || window.$IVIEW.size === ""
+          ? "default"
+          : window.$IVIEW.size;
       },
     },
     loading: Boolean,
     disabled: Boolean,
     htmlType: {
-      default: 'button',
+      default: "button",
       validator(value) {
-        return oneOf(value, ['button', 'submit', 'reset'])
+        return oneOf(value, ["button", "submit", "reset"]);
       },
     },
     icon: {
       type: String,
-      default: '',
+      default: "",
     },
     customIcon: {
       type: String,
-      default: '',
+      default: "",
     },
     long: {
       type: Boolean,
@@ -86,7 +101,7 @@ export default {
   },
   computed: {
     showSlot() {
-      return !!this.$slots.default
+      return !!this.$slots.default;
     },
     classes() {
       return [
@@ -95,44 +110,44 @@ export default {
         {
           [`${prefixCls}-long`]: this.long,
           [`${prefixCls}-${this.shape}`]: !!this.shape,
-          [`${prefixCls}-${this.size}`]: this.size !== 'default',
+          [`${prefixCls}-${this.size}`]: this.size !== "default",
           [`${prefixCls}-loading`]: this.loading != null && this.loading,
           [`${prefixCls}-icon-only`]:
             !this.showSlot &&
             (!!this.icon || !!this.customIcon || this.loading),
           [`${prefixCls}-ghost`]: this.ghost,
         },
-      ]
+      ];
     },
     // Point out if it should render as <a> tag
     isHrefPattern() {
-      const { to } = this
-      return !!to
+      const { to } = this;
+      return !!to;
     },
     tagName() {
-      const { isHrefPattern } = this
-      return isHrefPattern ? 'a' : 'button'
+      const { isHrefPattern } = this;
+      return isHrefPattern ? "a" : "button";
     },
     tagProps() {
-      const { isHrefPattern } = this
+      const { isHrefPattern } = this;
       if (isHrefPattern) {
-        const { linkUrl, target } = this
-        return { href: linkUrl, target }
+        const { linkUrl, target } = this;
+        return { href: linkUrl, target };
       } else {
-        const { htmlType } = this
-        return { type: htmlType }
+        const { htmlType } = this;
+        return { type: htmlType };
       }
     },
   },
   methods: {
     // Ctrl or CMD and click, open in new window when use `to`
     handleClickLink(event) {
-      this.$emit('click', event)
-      const openInNewWindow = event.ctrlKey || event.metaKey
+      this.$emit("click", event);
+      const openInNewWindow = event.ctrlKey || event.metaKey;
 
-      this.handleCheckClick(event, openInNewWindow)
+      this.handleCheckClick(event, openInNewWindow);
     },
   },
-  emits: ['click'],
-}
+  emits: ["click"],
+};
 </script>
