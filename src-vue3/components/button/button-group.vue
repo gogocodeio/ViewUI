5262b5106d16785197531a9f8a019654e10bfa11
chibing.fy <chibing.fy@alibaba-inc.com>
2021-08-04 17:12:03 +0800

    pretty

--- src-vue3/components/button/button-group.vue
+++ src-vue3/components/button/button-group.vue
@@ -1,45 +1,47 @@
 <template>
-    <div :class="classes">
-        <slot></slot>
-    </div>
+  <div :class="classes">
+    <slot></slot>
+  </div>
 </template>
 <script>
-    import { oneOf } from '../../utils/assist';
+import { oneOf } from '../../utils/assist'
 
-    const prefixCls = 'ivu-btn-group';
+const prefixCls = 'ivu-btn-group'
 
-    export default {
-        name: 'ButtonGroup',
-        props: {
-            size: {
-                validator (value) {
-                    return oneOf(value, ['small', 'large', 'default']);
-                },
-                default () {
-                    return !this.$IVIEW || this.$IVIEW.size === '' ? 'default' : this.$IVIEW.size;
-                }
-            },
-            shape: {
-                validator (value) {
-                    return oneOf(value, ['circle', 'circle-outline']);
-                }
-            },
-            vertical: {
-                type: Boolean,
-                default: false
-            }
+export default {
+  name: 'ButtonGroup',
+  props: {
+    size: {
+      validator(value) {
+        return oneOf(value, ['small', 'large', 'default'])
+      },
+      default() {
+        return !this.$IVIEW || this.$IVIEW.size === ''
+          ? 'default'
+          : this.$IVIEW.size
+      },
+    },
+    shape: {
+      validator(value) {
+        return oneOf(value, ['circle', 'circle-outline'])
+      },
+    },
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classes() {
+      return [
+        `${prefixCls}`,
+        {
+          [`${prefixCls}-${this.size}`]: !!this.size,
+          [`${prefixCls}-${this.shape}`]: !!this.shape,
+          [`${prefixCls}-vertical`]: this.vertical,
         },
-        computed: {
-            classes () {
-                return [
-                    `${prefixCls}`,
-                    {
-                        [`${prefixCls}-${this.size}`]: !!this.size,
-                        [`${prefixCls}-${this.shape}`]: !!this.shape,
-                        [`${prefixCls}-vertical`]: this.vertical
-                    }
-                ];
-            }
-        }
-    };
+      ]
+    },
+  },
+}
 </script>
